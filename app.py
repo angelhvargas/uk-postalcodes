0c08a181e811d31d7eb1abe6a7d691406eb26dce
Angel Vargas <angelvargas@outlook.es>
2018-05-18 15:01:19 +0100

    Adding routes to handle post request from the postcode parser tool

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-
-from flask import Flask, render_template
-import json
 import os
+import json
+import postcodes
 import mysql.connector
 from mysql.connector import errorcode
-import postcodes
+from flask import Flask, render_template, request
+
 
 try:
     cnx = mysql.connector.connect(user='root', password='1234567890', host='127.0.0.1', database='finance')
@@ -37,20 +37,36 @@ def multiples():
     return render_template('multiples.html', title='Multiples', multiples=multiples_)
 
 
-@app.route('/postal-codes')
+@app.route('/postal-codes', methods=['GET'])
 def postal_codes():
     def format_codes():
         filename = os.path.join(os.path.dirname(__file__), 'storage/postal_codes.json')
         with open(filename, encoding='utf-8') as postal_codes:
             data = json.loads(postal_codes.read())
         return data
-
     return render_template('postal-codes.html', title='UK - Postal Codes', postalcodes=format_codes())
+
+
+@app.route('/postal-codes', methods=['POST'])
+def postal_codes_form():
+    def process_form():
+        formatted = ''
+        valid_ps = False
+        error_message = '';
+        try:
+            original_input = request.form['postal_code']
+            formatted = ' '.join([str(part) for part in postcodes.parse_format(str(original_input))])
+            valid_ps = True
+        except ValueError as Exc:
+            error_message = Exc
+
+        return {'pristine': original_input, 'valid': valid_ps, 'formatted': formatted, 'error_message': error_message}
+    return render_template('postal-codes.html', title='UK - Postal Codes', postal_codes={}, post_results=process_form())
 
 
 @app.route('/postal-codes/<string:code_text>', methods=['GET'])
 def get_postcode(code_text):
-    search = postcodes.parse_format(code_text);
+    search = postcodes.parse_format(code_text)
 
     search = ' '.join([str(part) for part in search])
 
@@ -63,4 +79,4 @@ def get_postcode(code_text):
 
 
 if __name__ == '__main__':
-    app.run(debug=True  )
+    app.run(debug=True)
