d2a2e5b94b39437f6e172a6d090fe797beb6b436
Angel Vargas <angelvargas@outlook.es>
2018-05-21 01:39:32 +0100

    Fixed regex patterns to follow current UK standards.

--- postcodes.py
+++ postcodes.py
@@ -2,49 +2,78 @@ import re
 from functools import partial
 
 
-THIRD_PART_ONLY = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'S','T', 'U', 'W']
-FOURTH_PART_ONLY = ['A', 'B', 'E', 'H', 'M', 'N', 'P', 'R', 'V', 'W', 'X', 'Y']
-INCODE_PART = ['A', 'B', 'D', 'E', 'F', 'G', 'H', 'J', 'L', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z']
+INWARD_ELEM = ['A', 'B', 'D', 'E', 'F', 'G', 'H', 'J', 'L', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z']
 
-POSTAL_ZONES = ['AB', 'AL', 'B', 'BA', 'BB', 'BD', 'BH', 'BL', 'BN', 'BR', 'BS', 'BT', 'CA', 'CB', 'CF', 
+POSTAL_ZONES = ['AB', 'AL', 'B', 'BA', 'BB', 'BD', 'BH', 'BL', 'BN', 'BR', 'BS', 'BT', 'CA', 'CB', 'CF',
                 'CH', 'CM', 'CO', 'CR', 'CT', 'CV', 'CW', 'DA', 'DD', 'DE', 'DG', 'DH', 'DL', 'DN', 'DT',
-                'DY', 'E', 'EC', 'EH', 'EN', 'EX', 'FK', 'FY', 'G', 'GL', 'GY', 'GU', 'HA', 'HD', 'HG', 
+                'DY', 'E', 'EC', 'EH', 'EN', 'EX', 'FK', 'FY', 'G', 'GL', 'GY', 'GU', 'HA', 'HD', 'HG',
                 'HP', 'HR', 'HS', 'HU', 'HX', 'IG', 'IM', 'IP', 'IV', 'JE', 'KA', 'KT', 'KW', 'KY', 'L',
                 'LA', 'LD', 'LE', 'LL', 'LN', 'LS', 'LU', 'M', 'ME', 'MK', 'ML', 'N', 'NE', 'NG', 'NN',
                 'NP', 'NR', 'NW', 'OL', 'OX', 'PA', 'PE', 'PH', 'PL', 'PO', 'PR', 'RG', 'RH', 'RM', 'S',
-                'SA', 'SE', 'SG', 'SK', 'SL', 'SM', 'SN', 'SO', 'SP', 'SR', 'SS', 'ST', 'SW', 'SY', 'TA', 
+                'SA', 'SE', 'SG', 'SK', 'SL', 'SM', 'SN', 'SO', 'SP', 'SR', 'SS', 'ST', 'SW', 'SY', 'TA',
                 'TD', 'TF', 'TN', 'TQ', 'TR', 'TS', 'TW', 'UB', 'W', 'WA', 'WC', 'WD', 'WF', 'WN', 'WR',
                 'WS', 'WV', 'YO', 'ZE']
-# single character postal zone case
-SINGLE_CHAR_PS = [zone for zone in POSTAL_ZONES if len(zone) == 1]
-# double character postal zone case
-DOUBLE_CHAR_PS = [zone for zone in POSTAL_ZONES if len(zone) == 2]
-OUTCODE_REGX = (r'(' +
+
+# Areas with only single-digit districts
+SINGLE_DIGIT_AREAS = ['BR', 'FY', 'HA', 'HD', 'HG', 'HR', 'HS', 'HX', 'JE', 'LD', 'SM', 'SR', 'WC', 'WN', 'ZE']
+
+# Areas with only double-digit districts
+DOUBLE_DIGIT_AREAS = ['AB', 'LL', 'SO']
+
+# letters to exclude in first position
+FIRST_POSITION_EXCLUDED = ['Q', 'V', 'X']
+
+# letters to be excluded in second position
+SECOND_POSITION_EXCLUDED = ['I', 'J', 'Z']
+
+# The only letters to appear in the third position
+THIRD_PART_ONLY = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'S', 'T', 'U', 'W']
+
+# The only letters to appear in the fourth position
+FOURTH_PART_ONLY = ['A', 'B', 'E', 'H', 'M', 'N', 'P', 'R', 'V', 'W', 'X', 'Y']
+
+# Areas with a district '0' (zero). We exclude BS as this is the only one which accepts 10 and goes up-to 99
+DISTRICT_ZERO_AREAS = ['BL', 'CM', 'CR', 'FY', 'HA', 'PR', 'SL', 'SS']
+
+SPECIAL_AREA_WITH_TEN = ['BS']
+
+# single character postal zone cases
+SINGLE_CHAR_PC = [zone for zone in POSTAL_ZONES if len(zone) == 1]
+# double character postal zone cases
+DOUBLE_CHAR_PC = [zone for zone in POSTAL_ZONES if len(zone) == 2
+                  and zone not in (SINGLE_DIGIT_AREAS + DOUBLE_DIGIT_AREAS + DISTRICT_ZERO_AREAS + SPECIAL_AREA_WITH_TEN)]
+# outcode
+OUTWARD_PATTERN = (r'(' +
                    r'(?:(?:' +
-                   '|'.join(SINGLE_CHAR_PS) +
+                   '|'.join(SINGLE_CHAR_PC) +
                    r')(?:\d[' +
                    ''.join(THIRD_PART_ONLY) +
                    r']|\d{1,2}))' +
                    r'|' +
                    r'(?:(?:' +
-                   '|'.join(DOUBLE_CHAR_PS) +
+                   '|'.join(DOUBLE_CHAR_PC) +
                    r')(?:\d[' +
                    ''.join(FOURTH_PART_ONLY) +
-                   r']|\d{1,2}))' +
+                   r']|[1-9]{1,2}))' +
+                   r'|(?:(?:' + '|'.join(SINGLE_DIGIT_AREAS) + ')\d{1})'
+                   r'|(?:(?:' + '|'.join(DOUBLE_DIGIT_AREAS) + ')(?:[1-9][1-9]{0,2}))'
+                   r'|(?:(?:' + '|'.join(DISTRICT_ZERO_AREAS) + ')[0-9])'
+                   r'|(?:BS(?:[1-9][0-9]))'
                    r')')
-INCODE_PATTERN = (r'(\d[' +
-                  ''.join(INCODE_PART) +
+# incode
+INWARD_PATTERN = (r'(\d[' +
+                  ''.join(INWARD_ELEM) +
                   r'][' +
-                  ''.join(INCODE_PART) +
+                  ''.join(INWARD_ELEM) +
                   r'])')
-POSTCODE_PATTERN = OUTCODE_REGX + INCODE_PATTERN
-STANDALONE_OUTCODE_PATTERN = OUTCODE_REGX + r'\s*$'
+POSTCODE_PATTERN = OUTWARD_PATTERN + INWARD_PATTERN
+STANDALONE_OUTWARD_PATTERN = OUTWARD_PATTERN + r'\s*$'
 
 POSTCODE_REGEX = re.compile(POSTCODE_PATTERN)
-STANDALONE_OUTCODE_REGEX = re.compile(STANDALONE_OUTCODE_PATTERN)
+STANDALONE_OUTWARD_REGEX = re.compile(STANDALONE_OUTWARD_PATTERN)
 
-
-def postcode_parser(postcode, incode_mandatory=True):
+print(POSTCODE_PATTERN)
+def postcode_parser(postcode, inward_mandatory=True):
     postcode = postcode.replace(' ', '').upper()
 
     if len(postcode) > 7:
@@ -54,20 +83,20 @@ def postcode_parser(postcode, incode_mandatory=True):
     if postcode_match:
         return postcode_match.group(1, 2)
 
-    outcode_match = STANDALONE_OUTCODE_REGEX.match(postcode)
+    outcode_match = STANDALONE_OUTWARD_REGEX.match(postcode)
     if outcode_match:
-        if incode_mandatory:
+        if inward_mandatory:
             raise ValueError('The incode is required')
         else:
             return outcode_match.group(1), ''
 
-    if postcode == 'TS97BQ':
-        return 'TS9', '7BQ'
-    elif postcode == 'TS9':
-        if incode_mandatory:
+    if postcode == 'GIR0AA':
+        return 'GIR', '0AA'
+    elif postcode == '0AA':
+        if inward_mandatory:
             raise ValueError('The Incode is required')
         else:
-            return 'TS9', ''
+            return '0AA', ''
 
     raise ValueError('UK postal code invalid')
 
